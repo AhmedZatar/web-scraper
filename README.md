2eea8fdfa82a939e95a589ac3df84ee68b3dd463
ahmedzatar <ahmed.zatar2@gmail.com>
2021-06-29 10:18:49 +0300

    finish lab17

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[https://github.com/AhmedZatar/web-scraper/pull/1](https://github.com/AhmedZatar/web-scraper/pull/1)
+[https://github.com/AhmedZatar/web-scraper/pull/2](https://github.com/AhmedZatar/web-scraper/pull/2)
--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -19,8 +19,27 @@ def get_citations_needed_report(url):
     for p in all_p:
         has_citations =p.findAll('a',title='Wikipedia:Citation needed')
         if len(has_citations)>0:
-            for i in has_citations:
-                all_p_has_citations.append(p.text.strip()) 
+              for i in has_citations:
+                text=''
+                for i in range(len(p.text.strip())) :
+                    if p.text.strip()[i]=='[':
+                        break
+                    else:
+                        text+=p.text.strip()[i]
+                if not text in all_p_has_citations:
+                  all_p_has_citations.append(text)
+                else: 
+                    text=''
+                    for i in range(len(p.text.strip())) :
+                      if p.text.strip()[i]==']':
+                        for j in range(i+1,len(p.text.strip())) :
+                            if p.text.strip()[j]=='[':
+                              break
+                            else:
+                                text+=p.text.strip()[j]
+                        break
+                    all_p_has_citations.append(text)
+                                 
 
     return "\n\n".join(all_p_has_citations)
 
